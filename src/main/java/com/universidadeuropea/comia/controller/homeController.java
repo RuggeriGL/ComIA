8c7368b59e72798c7495a6a30dcb8aedc2e0cb31
BernardoManuel <96483675+BernardoManuel@users.noreply.github.com>
2024-03-14 14:36:00 +0100

    Fixed dependencies

--- src/main/java/com/universidadeuropea/comia/controller/homeController.java
+++ src/main/java/com/universidadeuropea/comia/controller/homeController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class homeController {
 
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	@RequestMapping(value = { "/home", "/" }, method = RequestMethod.GET)
 	public String home() {
 
-		return "home";
+		return "home.html";
 	}
 
 }
